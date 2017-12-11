ddb8fa36758dbde868b4bc8250488494268e4d71
Sergei Volodin <sergei.volodin@epfl.ch>
2017-12-11 23:59:44 +0000

    Fix memory issue for glove cooc

--- sample_code/cooc.py
+++ sample_code/cooc.py
@@ -2,6 +2,7 @@
 from scipy.sparse import *
 import numpy as np
 import pickle
+from tqdm import tqdm
 
 
 def main():
@@ -9,25 +10,25 @@ def main():
         vocab = pickle.load(f)
     vocab_size = len(vocab)
 
-    data, row, col = [], [], []
+    arr = {}
     counter = 1
     for fn in ['pos_train.txt', 'neg_train.txt']:
+        num_lines = 0
         with open(fn) as f:
             for line in f:
+                num_lines += 1
+        with open(fn) as f:
+            for line in tqdm(f, total = num_lines):
                 tokens = [vocab.get(t, -1) for t in line.strip().split()]
                 tokens = [t for t in tokens if t >= 0]
                 for t in tokens:
                     for t2 in tokens:
-                        data.append(1)
-                        row.append(t)
-                        col.append(t2)
+                        if (t, t2) not in arr: arr[(t, t2)] = 1
+                        else: arr[(t, t2)] += 1
 
-                if counter % 10000 == 0:
-                    print(counter)
                 counter += 1
-    cooc = coo_matrix((data, (row, col)))
-    print("summing duplicates (this can take a while)")
-    cooc.sum_duplicates()
+
+    cooc = coo_matrix(([1] * len(arr), ([x for x, y in arr.keys()], [y for x, y in arr.keys()])))
     with open('cooc.pkl', 'wb') as f:
         pickle.dump(cooc, f, pickle.HIGHEST_PROTOCOL)
 

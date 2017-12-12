791e070fbf78b765b2d288cf358f89b8256bbd3e
Sergei Volodin <sergei.volodin@epfl.ch>
2017-12-12 07:55:34 +0000

    Fix data always 1; Fix #11

--- glove/cooc.py
+++ glove/cooc.py
@@ -11,7 +11,6 @@ def main():
     vocab_size = len(vocab)
 
     arr = {}
-    counter = 1
     for fn in ['pos_train.txt', 'neg_train.txt']:
         num_lines = 0
         with open(fn) as f:
@@ -23,12 +22,14 @@ def main():
                 tokens = [t for t in tokens if t >= 0]
                 for t in tokens:
                     for t2 in tokens:
-                        if (t, t2) not in arr: arr[(t, t2)] = 1
-                        else: arr[(t, t2)] += 1
+                        if (t, t2) not in arr:
+                            arr[(t, t2)] = 1
+                        else:
+                            arr[(t, t2)] += 1
 
-                counter += 1
-
-    cooc = coo_matrix(([1] * len(arr), ([x for x, y in arr.keys()], [y for x, y in arr.keys()])))
+    keys = list(arr.keys())
+    values = [arr[k] for k in keys]
+    cooc = coo_matrix((values, ([x for x, y in keys], [y for x, y in keys])))
     with open('cooc.pkl', 'wb') as f:
         pickle.dump(cooc, f, pickle.HIGHEST_PROTOCOL)
 
